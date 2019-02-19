252fc888dacc0c57bc7be91466f3d8bca77afc49
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2019-02-19 23:51:46 +0100

    KPB: add cmocka test cases for real time copy and internal buffering
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

---
+++ test/cmocka/src/audio/kpb/kpb_mock.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include <sof/alloc.h>
+#include <sof/audio/component.h>
+#include <mock_trace.h>
+
+TRACE_IMPL()
+
+void *rballoc(int zone, uint32_t caps, size_t bytes)
+{
+	(void)zone;
+	(void)caps;
+
+	return malloc(bytes);
+}
+
+void *rzalloc(int zone, uint32_t caps, size_t bytes)
+{
+	(void)zone;
+	(void)caps;
+
+	return calloc(bytes, 1);
+}
+
+void rfree(void *ptr)
+{
+	free(ptr);
+}
+
+void pipeline_xrun(struct pipeline *p, struct comp_dev *dev, int32_t bytes)
+{
+}
+
+int comp_set_state(struct comp_dev *dev, int cmd)
+{
+	return 0;
+}
+
+void work_schedule_default(struct work *w, uint64_t timeout)
+{
+}
