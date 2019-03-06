4055a8ad6afe3d1c17ab1b2e0cb1cd0fa861a937
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-03-05 21:24:37 -0800

    uapi: align comp types
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/include/uapi/ipc/topology.h
+++ src/include/uapi/ipc/topology.h
@@ -63,12 +63,11 @@ enum sof_comp_type {
 	SOF_COMP_EQ_IIR,
 	SOF_COMP_EQ_FIR,
 	SOF_COMP_KEYWORD_DETECT,
-	SOF_COMP_VOICE_DETECT,
-	/* keep FILEREAD/FILEWRITE as the last ones */
-	SOF_COMP_FILEREAD,	/**< host test based file IO */
-	SOF_COMP_FILEWRITE,	/**< host test based file IO */
 	SOF_COMP_KPB, /* A key phrase buffer component */
-	SOF_COMP_SELECTOR,
+	SOF_COMP_SELECTOR, /**< channel selector */
+	/* keep FILEREAD/FILEWRITE as the last ones */
+	SOF_COMP_FILEREAD = 10000,	/**< host test based file IO */
+	SOF_COMP_FILEWRITE = 10001,	/**< host test based file IO */
 };
 
 /* XRUN action for component */
